362693e9d228dd65bfbb88db9c82f313064d506c
aqilmustaqim <aqilmustaqim28@gmail.com>
2022-12-29 12:47:58 +0700

    Update Fitur Login ( Selesai )

--- app/Controllers/Auth.php
+++ app/Controllers/Auth.php
@@ -2,8 +2,18 @@
 
 namespace App\Controllers;
 
+use App\Models\UsersModel;
+
 class Auth extends BaseController
 {
+
+    protected $usersModel;
+
+    public function __construct()
+    {
+        $this->usersModel = new UsersModel();
+    }
+
     public function index()
     {
         $data = [
@@ -11,5 +21,109 @@ class Auth extends BaseController
             'validation' => \Config\Services::validation()
         ];
         return view('auth/login', $data);
+    }
+
+    public function loginSave()
+    {
+
+        //Validasi Form Terlebih Dahulu
+        if (!$this->validate([
+            //Field Yang mau divalidasi
+            'email' => [
+                'rules' => 'required',
+                'errors' => [
+                    'required' => '{field} Wajib Diisi ! '
+                ]
+            ],
+            'password' => [
+                'rules' => 'required',
+                'errors' => [
+                    'required' => '{field} Wajib Diisi ! '
+                ]
+            ]
+        ])) {
+            //Kalau tidak tervalidasi
+            return redirect()->to(base_url())->withInput();
+        }
+
+        //Ambil Data Form
+        $email = $this->request->getVar('email');
+        $password = $this->request->getVar('password');
+
+        //Cek Data Login
+        $user = $this->usersModel->where(['email' => $email])->first();
+
+        //Validasi Loginnya
+        if ($user) {
+            //Jika Ada Usernya
+            //1.Cek Apakah Passwordnya Benar
+            if ($user['password'] == password_verify($password, $user['password'])) {
+                //Kalau Benar Cek Usernya Aktif Gak
+                if ($user['is_active'] == 1) {
+                    //Kalau Aktif Kasi Session
+                    //1.Simpan session usernya
+                    $dataSession = [
+                        'nama' => $user['nama'],
+                        'email' => $user['email'],
+                        'role_id' => $user['role_id'],
+                        'foto' => $user['foto'],
+                        'logged_in' => TRUE
+                    ];
+                    session()->set($dataSession);
+
+                    //2. Redirect Ke Halaman Masing Masing
+                    if ($user['role_id'] == 1) {
+                        return redirect()->to(base_url('admin'));
+                    } else if ($user['role_id'] == 2) {
+                        return redirect()->to(base_url('waiters'));
+                    } else if ($user['role_id'] == 3) {
+                        return redirect()->to(base_url('koki'));
+                    } else if ($user['role_id'] == 4) {
+                        return redirect()->to(base_url('kasir'));
+                    }
+                } else {
+                    //litertly wicis if user tidak aktif
+                    session()->setFlashdata('login', 'Akun anda Belum Aktif, harap hubungi admin ! ');
+                    return redirect()->to(base_url());
+                }
+            } else {
+                session()->setFlashdata('login', 'Password Salah ! ');
+                return redirect()->to(base_url());
+            }
+        } else {
+            //Jika Tidak
+            session()->setFlashdata('login', 'Email Belum Terdaftar ! ');
+            return redirect()->to(base_url());
+        }
+    }
+
+    public function logout()
+    {
+        //Hancurkan Session
+        $dataSession = [
+            'nama',
+            'email',
+            'role_id',
+            'foto',
+            'logged_in'
+        ];
+        session()->remove($dataSession);
+        return redirect()->to(base_url());
+    }
+
+    public function register()
+    {
+        $password = 'aqilmustaqim16';
+
+        if ($this->usersModel->save([
+            'nama' => 'Aqil Mustaqim',
+            'email' => 'aqilmustaqim28@gmail.com',
+            'password' => password_hash($password, PASSWORD_DEFAULT),
+            'role_id' => 1,
+            'foto' => 'default.png',
+            'is_active' => 1
+        ])) {
+            echo 'berhasil';
+        }
     }
 }
